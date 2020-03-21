56110b7b9994e6fe496bf7cbf33e5636eacc6102
Shuai Zhang <2892928163@qq.com>
2020-03-21 15:20:29 +0800

    update fileinfo.py

--- fileinfo.py
+++ fileinfo.py
@@ -10,3 +10,11 @@
 # get file information using os.stat()
 # tested with Python24 vegsaeat 25sep2006
 from __future__ import print_function
+
+import os
+import stat # index constants for os.stat()
+import sys
+import time
+
+if sys.version_info >= (3, 0):
+    raw_input = input
